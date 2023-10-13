c77cca30e8640d009252d720ef50a18648c72b23
iskandervulto <132972414+iskandervulto@users.noreply.github.com>
2023-10-13 19:16:24 -0400

    These got foresaken patches are done

--- client/src/components/EditUser.js
+++ client/src/components/EditUser.js
@@ -1,63 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react';
+import InjuryDropdownOptions from './InjuryDropDownOptions'; // You may need to adjust the import path
+import DropdownOptions from './DropdownOptions'; // You may need to adjust the import path
 
-import DropdownOptions from "../DropdownOptions";
-import InjuryDropdownOptions from "../InjuryDropDownOptions";
+function EditUser({ option }) {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
 
-function EditUser() {
-    const [formData, setFormData] = useState({
-        username: '',
-        password: '',
+  const [injuryId, setInjuryId] = useState(0);
+  const [strengtheningId, setStrengtheningId] = useState(0);
+  const [mobilityId, setMobilityId] = useState(0);
+  const [flexibilityId, setFlexibilityId] = useState(0);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    if (name === 'username' || name === 'password') {
+      setFormData({ ...formData, [name]: value });
+    }
+  };
+
+  const handleInjuryChange = (e) => {
+    const selectedValue = e.target.value;
+    setInjuryId(selectedValue);
+  };
+
+  const handleStrengtheningChange = (e) => {
+    const selectedValue = e.target.value;
+    setStrengtheningId(selectedValue);
+  };
+
+  const handleMobilityChange = (e) => {
+    const selectedValue = e.target.value;
+    setMobilityId(selectedValue);
+  };
+
+  const handleFlexibilityChange = (e) => {
+    const selectedValue = e.target.value;
+    setFlexibilityId(selectedValue);
+  };
+
+  const formDataWithIds = {
+    ...formData,
+    injury_id: injuryId,
+    strengthening_id: strengtheningId,
+    mobility_id: mobilityId,
+    flexibility_id: flexibilityId,
+  };
+
+  const editUser = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/users/${option}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formDataWithIds),
+
       });
-    
-      const [injuryId, setInjuryId] = useState(0);
-      const [strengtheningId, setStrengtheningId] = useState(0);
-      const [mobilityId, setMobilityId] = useState(0);
-      const [flexibilityId, setFlexibilityId] = useState(0);
-    
-      const handleChange = (e) => {
-        const selectedValue = e.target.value;
-        const { name, value } = e.target;
-    
-        console.log(selectedValue);
-      
-        if (name === "username" || name === "password") {
-          setFormData({ ...formData, [name]: value });
-        }
-      };
-    
-      const handleInjuryChange = (e) => {
-        const selectedValue = e.target.value;
-        console.log(selectedValue);
-        setInjuryId(selectedValue);
-      };
-      
-      const handleStrengtheningChange = (e) => {
-        const selectedValue = e.target.value;
-        console.log(selectedValue);
-        setStrengtheningId(selectedValue);
-      };
-      
-      const handleMobilityChange = (e) => {
-        const selectedValue = e.target.value;
-        console.log(selectedValue);
-        setMobilityId(selectedValue);
-      };
-      
-      const handleFlexibilityChange = (e) => {
-        const selectedValue = e.target.value;
-        console.log(selectedValue);
-        setFlexibilityId(selectedValue);
-      };
 
-      const handleSubmit = (e) => {
-        e.preventDefault();
-      };
-  
-    return (
+      if (response.ok) {
+        // Handle successful PATCH request (e.g., update state or navigate)
+        console.log('User updated successfully');
+      } else {
+        console.error('Error editing user:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error editing user:', error);
+    }
+
+    console.log(option)
+  };
+
+  return (
+    <div>
+      <form>
         <div>
-        <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="firstName">Username:</label>
+          <label htmlFor="username">Username:</label>
           <input
             type="text"
             id="username"
@@ -68,7 +88,7 @@ function EditUser() {
           />
         </div>
         <div>
-          <label htmlFor="password">Password: </label>
+          <label htmlFor="password">Password:</label>
           <input
             type="password"
             id="password"
@@ -78,40 +98,31 @@ function EditUser() {
             required
           />
         </div>
-     
-           {/* DROP DOWNS START HERE */}
 
-      <div>
-        <label htmlFor="injury_id">Injury:</label>
-        <InjuryDropdownOptions type="injuries" onChange={handleInjuryChange} />
-      </div>
-
-      <div>
-        <label htmlFor="strengthening_id">Strengthening:</label>
-        <DropdownOptions type="strengthenings" onChange={handleStrengtheningChange} />
-      </div>
-
-      <div>
-        <label htmlFor="mobility_id">Mobility:</label>
-        <DropdownOptions type="mobilities" onChange={handleMobilityChange} />
-      </div>
-
-      <div>
-        <label htmlFor="flexibility_id">Flexibility:</label>
-        <DropdownOptions type="flexibilities" onChange={handleFlexibilityChange} />
-      </div>
-
-        {/* Button is here */}
-        
         <div>
-          <button type="submit">Submit</button>
+          <label htmlFor="injury_id">Injury:</label>
+          <InjuryDropdownOptions type="injuries" onChange={handleInjuryChange} />
         </div>
+
+        <div>
+          <label htmlFor="strengthening_id">Strengthening:</label>
+          <DropdownOptions type="strengthenings" onChange={handleStrengtheningChange} />
+        </div>
+
+        <div>
+          <label htmlFor="mobility_id">Mobility:</label>
+          <DropdownOptions type="mobilities" onChange={handleMobilityChange} />
+        </div>
+
+        <div>
+          <label htmlFor="flexibility_id">Flexibility:</label>
+          <DropdownOptions type="flexibilities" onChange={handleFlexibilityChange} />
+        </div>
+
+        <button onClick={editUser}>Submit</button>
       </form>
     </div>
   );
 }
 
-  
-  
-  export default EditUser;
-  
+export default EditUser;
--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 
+import EditUser from "../EditUser";
 
 import "./styles/AllPages.css";
 
@@ -14,6 +15,7 @@ const Profile = () => {
       flexibility: "",
     },
   ]);
+
 
   useEffect(() => {
     fetchAllUsers();
@@ -34,13 +36,17 @@ const Profile = () => {
     }
   };
 
+
+
+
   // Patch function
 
-  const handleEditUser = (userId) => {
-  
-    const editedUserData = users.find((user) => user.id === userId);
+  const [isEditing, setIsEditing] = useState(false)
+  const [selectedUserId, setSelectedUserId] = useState(null);
 
-    editUser(editedUserData); 
+  const handleEditUser = (userId) => {
+    setIsEditing(true);
+    setSelectedUserId(userId);
   };
 
   const editUser = async (userData) => {
@@ -85,8 +91,8 @@ const Profile = () => {
     <div className="User">
       <header className="User-header">
         <h1>Profile</h1>
-        {users.map((user, index) => (
-          <div key={index} className="All-container">
+        {users.map((user) => (
+          <div key={user.id} className="All-container">
             <p>Username: {user.username}</p>
             <p>Password: {user.password}</p>
             <p>Injury: {user.injury_id}</p>
@@ -95,6 +101,9 @@ const Profile = () => {
             <p>Flexibility: {user.flexibility_id}</p>
             <button onClick={() => handleEditUser(user.id)}>Edit</button>
             <button onClick={() => handleDeleteUser(user.id)}>Delete</button>
+            {isEditing && selectedUserId === user.id && (
+              <EditUser option={user.id} key={user.id} /> // Pass user as the option
+            )}
           </div>
         ))}
       </header>
